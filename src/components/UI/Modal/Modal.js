1181596aa0d2407e999fcff634572b35c7b28789
aindriu80 <aindriu80@gmail.com>
2019-07-18 21:18:46 +0100

    Implementing the Backdrop Component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 import classes from './Modal.css';
+import Aux from '../../../hoc/Auxillary';
+import Backdrop from '../Backdrop/Backdrop';
+
 const modal = (props) => (
+    <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
     <div 
         className={classes.Modal}
         style={{
@@ -10,6 +15,7 @@ const modal = (props) => (
         }}>
         {props.children}
     </div>
+    </Aux>
 );
 
 export default modal;
