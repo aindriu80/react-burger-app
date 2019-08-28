e7f2020bc4d698042c15d8ba767b589a8eab0ca0
aindriu80 <aindriu80@gmail.com>
2019-08-28 20:45:17 +0100

    Adding custom form validation in React

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,8 +15,10 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-
-                value: ''
+                value: '',
+                validation: {
+                    required:true
+                },valid:false
                 },  
                 street:{
                     elementType: 'input',
@@ -24,7 +26,10 @@ class ContactData extends Component {
                         type: 'text',
                         placeholder: 'Street'
                     },
-                    value: ''
+                    value: '',
+                    validation: {
+                        required:true
+                    },valid:false
                 },   
                 zipCode:{
                     elementType: 'input',
@@ -32,7 +37,12 @@ class ContactData extends Component {
                         type: 'text',
                         placeholder: 'Zip Code'
                     },
-                    value: ''
+                    value: '',
+                    validation: {
+                        required:true,
+                        minLength: 5,
+                        maxLength: 5
+                    },valid:false
                 },
                 country:{
                     elementType: 'input',
@@ -40,7 +50,10 @@ class ContactData extends Component {
                         type: 'text',
                         placeholder: 'Country'
                     },
-                    value: ''
+                    value: '',
+                    validation: {
+                        required:true
+                    },valid:false
                 },
                 email:{
                     elementType: 'input',
@@ -48,7 +61,10 @@ class ContactData extends Component {
                         type: 'email',
                         placeholder: 'Your Email'
                     },
-                    value: ''
+                    value: '',
+                    validation: {
+                        required:true
+                    },valid:false
                 },
                 deliveryMethod:{
                     elementType: 'select',
@@ -90,6 +106,25 @@ class ContactData extends Component {
             });
     }
 
+    checkValidity(value, rules) {
+    let isValid = false;    
+
+        if (rules.required) {
+            isValid = value.trim() !=='';
+        }
+
+        if (rules.minLength){
+            isValid = value.length >= rules.minLength
+        }
+
+        if (rules.maxLength){
+            isValid = value.length <= rules.maxLength
+        }
+    
+    
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -98,7 +133,9 @@ class ContactData extends Component {
             ...updatedOrderForm[inputIdentifier]
         };
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
         this.setState({orderForm: updatedOrderForm});
     }
 
