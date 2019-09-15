fa66fdd75a6ba9834998cac6603ec44287a36cd4
aindriu80 <aindriu80@gmail.com>
2019-09-15 20:06:02 +0100

    Creating Action Creators

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from './../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from './../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
-import * as actionType from '../../store/actions/actionTypes';
+import * as burgetBuilderActions from '../../store/actions/index';
 
 
 
@@ -120,8 +120,8 @@ const mapStateToProps = state => {
 }
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded: (ingName) => dispatch({type:actionType.ADD_INGREDIENT, ingredientName: ingName }),
-        onIngredientRemoved: (ingName) => dispatch({type:actionType.REMOVE_INGREDIENT, ingredientName: ingName })
+        onIngredientAdded: (ingName) => dispatch(burgetBuilderActions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName) => dispatch(burgetBuilderActions.removeIngredient(ingName))
     }
 }
 
