4dcecd6589211ecdaa829c0439f78d42f8ff976f
aindriu80 <aindriu80@gmail.com>
2019-09-21 21:33:48 +0100

    Handling the price after purchases

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from './../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from './../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
-import * as burgetBuilderActions from '../../store/actions/index';
+import * as actions from '../../store/actions/index';
 
 
 
@@ -48,14 +48,8 @@ class BurgerBuilder extends Component{
         this.setState({purchasing: false});
     }
 
-    purchaseContinueHandler = () => {       
-        // const queryParams = [];
-        // for (let i in this.state.ingredients){
-        //     queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        // }
-        // queryParams.push('price=' + this.state.totalPrice);
-        // const queryString = queryParams.join('&');
-
+    purchaseContinueHandler = () => {
+        this.props.onInitPurchase();       
         this.props.history.push('/checkout');
     }
 
@@ -111,9 +105,10 @@ const mapStateToProps = state => {
 }
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded: (ingName) => dispatch(burgetBuilderActions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) => dispatch(burgetBuilderActions.removeIngredient(ingName)),
-        onInitIngredients: () => dispatch(burgetBuilderActions.initIngredients())
+        onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(actions.initIngredients()),
+        onInitPurchase: () => dispatch(actions.purchaseInit())
     }
 }
 
