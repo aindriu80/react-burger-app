62f02a1816f026af4541c9a7b337ee0def81d392
aindriu80 <aindriu80@gmail.com>
2019-09-26 21:52:14 +0100

    Getting a token from the backend

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import Input from '../../components/UI/Input/Input'
-import Button from './../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 
@@ -113,7 +113,7 @@ const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email, password) => dispatch(actions.auth(email, password))
     };
-} ;
+};
 
 
 export default connect(null, mapDispatchToProps)(Auth);
