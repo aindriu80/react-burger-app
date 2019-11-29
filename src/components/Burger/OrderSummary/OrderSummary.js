53be33236008c9b33652326e201624d6e28b4db8
aindriu <aindriu80@gmail.com>
2019-11-29 22:07:34 +0000

    More hooks & React.memo()

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Auxillary from '../../../hoc/Auxillary/Auxillary';
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component {     
-    render () {
-        const ingredientSummary = Object.keys(this.props.ingredients)
-        .map(igKey => {
-            return (
-            <li key={igKey}>
-                {igKey}: {this.props.ingredients[igKey]}
-            </li>);
-    });        
-        return (
-            <Auxillary>
-            <h3>Your Order</h3>
-            <p>a delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
-        </Auxillary>
-        );
-    }
-} 
+const orderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+    return (
+      <li key={igKey}>
+        {igKey}: {props.ingredients[igKey]}
+      </li>
+    );
+  });
+  return (
+    <Auxillary>
+      <h3>Your Order</h3>
+      <p>a delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        Cancel
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        Continue
+      </Button>
+    </Auxillary>
+  );
+};
 
-export default OrderSummary;
+export default orderSummary;
